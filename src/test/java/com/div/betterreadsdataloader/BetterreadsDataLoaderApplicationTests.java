82a158b6202fae489bbe1451fceb3616dac5226a
Vidheesh Nacode <vidheeshnacode@hedwig.lan>
2022-01-31 01:00:46 -0500

    Setting up Connection with Cassandra

---
+++ src/test/java/com/div/betterreadsdataloader/BetterreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.div.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
